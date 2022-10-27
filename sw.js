c7a4dc81b03444b55b7463400c20abe5b3dafdad
Nickname <47962021+nicknamekr@users.noreply.github.com>
2022-10-27 16:55:51 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,11 @@
-const offlineFallbackPage = "offline.html"
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('sw.js?v2', {
+        scope: '.' // <--- THIS BIT IS REQUIRED
+    }).then(function(registration) {
+        // Registration was successful
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+        // registration failed :(
+        console.log('ServiceWorker registration failed: ', err);
+    });
+}
